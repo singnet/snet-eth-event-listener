953cbc48e20d34d5146ba3d376f3489155f71355
Kirill Dvoretskov <65371121+kiruxaspb@users.noreply.github.com>
2023-08-16 14:57:49 +0300

    Update ethereum event listener for mainnet (#25)

--- common/blockchain_util.py
+++ common/blockchain_util.py
@@ -1,8 +1,6 @@
-import web3
-import io
 import json
 from web3 import Web3
-from config import EthereumNetwork, HttpProvider, WebSocketProvider
+from config import EthereumNetwork, HttpProvider
 from common.logger import get_logger
 
 logger = get_logger(__name__)
@@ -16,23 +14,19 @@ class BlockChainUtil:
         self.web3_object = Web3(self.provider)
 
     def test_connection(self):
-        connected = True
         try:
-            connected = self.web3_object.isConnected()
+            connected = self.web3_object.is_connected()
+            assert connected, "Failed connection in test_connection"
         except Exception as e:
             logger.info(f"Error while testing web3 connection {repr(e)}")
-            connected = False
-        if not connected:
-            self.reset_connection()
 
     def reset_connection(self):
-        self.test_connection()
         self.provider = getattr(Web3, getattr(self.provider_class, "web3_class"))(self.provider_url)
         self.web3_object = Web3(self.provider)
 
     def get_current_block_no(self):
         self.test_connection()
-        return self.web3_object.eth.blockNumber
+        return self.web3_object.eth.block_number
 
     def create_contract_instance(self, abi, network_address):
         self.test_connection()
@@ -40,4 +34,4 @@ class BlockChainUtil:
 
     def get_transaction_receipt(self, transaction_hash):
         self.test_connection()
-        return self.web3_object.eth.getTransactionReceipt(transaction_hash)
+        return self.web3_object.eth.get_transaction_receipt(transaction_hash)
